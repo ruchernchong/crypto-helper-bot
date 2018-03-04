d0b002de2f74a4daef7c883abcb25ca577a638fd
Ru Chern Chong <me@ruchern.com>
2018-03-05 00:24:00 +0800

    Add command to retrieve price of a particular coin
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ const bot = new TelegramBot(token, {polling: true})
 
 axios.defaults.headers.common['user-agent'] = 'Crypto Helper/1.0.0'
 
-const BASE_URL = 'https://coinmarketcal.com'
+const CAL_BASE_URL = 'https://coinmarketcal.com'
+const COIN_BASE_URL = 'https://coincap.io'
 
 let coinList
 
-axios.get(`${BASE_URL}/api/coins`).then(response => {
+axios.get(`${CAL_BASE_URL}/api/coins`).then(response => {
   coinList = response.data
   console.log('Data has been assigned to global variable')
 }).catch(error => {
@@ -21,7 +22,7 @@ bot.onText(/\/events/, message => {
 
   let strEvent
 
-  axios.get(`${BASE_URL}/api/events`, {
+  axios.get(`${CAL_BASE_URL}/api/events`, {
     params: {
       max: 3
     }
@@ -37,7 +38,7 @@ bot.onText(/\/events/, message => {
       strEvent += `${coinName} (${coinSymbol})\nTitle: ${event.title}\nDate: ${new Date(event.date_event).toLocaleDateString()}\nMore details: ${event.source}\n\n`
     })
 
-    strEvent += `More events can be found on ${BASE_URL}`
+    strEvent += `More events can be found on ${CAL_BASE_URL}`
 
     bot.sendMessage(chatId, strEvent, {
       disable_web_page_preview: true,
@@ -53,7 +54,7 @@ bot.onText(/\/event (.+)/, (message, match) => {
   const index = coinList.findIndex(list => list.includes(coin))
 
   if (index > -1) {
-    axios.get(`${BASE_URL}/api/events`, {
+    axios.get(`${CAL_BASE_URL}/api/events`, {
       params: {
         coins: coinList[index],
         max: 1
@@ -64,7 +65,7 @@ bot.onText(/\/event (.+)/, (message, match) => {
       let reply
 
       if (response.data.length > 0) {
-        reply = `Here is an upcoming event for ${coinList[index]}:\n\nThis event has been voted: ${event.positive_vote_count}/${event.vote_count} (${event.percentage}%)\n\nTitle: ${event.title}\nDate: ${new Date(event.date_event).toLocaleDateString()}\nDescription: ${event.description}\nSource: ${event.source}\n${BASE_URL}`
+        reply = `Here is an upcoming event for ${coinList[index]}:\n\nThis event has been voted: ${event.positive_vote_count}/${event.vote_count} (${event.percentage}%)\n\nTitle: ${event.title}\nDate: ${new Date(event.date_event).toLocaleDateString()}\nDescription: ${event.description}\nSource: ${event.source}\n${CAL_BASE_URL}`
       } else {
         reply = `There are no event(s) for ${coinList[index]}.`
       }
@@ -81,4 +82,24 @@ bot.onText(/\/event (.+)/, (message, match) => {
       reply_to_message_id: message.message_id
     }).then(() => console.log(`Unable to find ${coin}.`))
   }
+})
+
+bot.onText(/\/coin (.+)/, (message, match) => {
+  const chatId = message.chat.id
+  const coin = match[1]
+
+  axios.get(`${COIN_BASE_URL}/page/${coin}`).then(response => {
+    const data = response.data
+
+    const rank = `*Rank:* _${data.rank}_`
+    const mCap = `*Est. Market Cap:* _${data.market_cap.toLocaleString('en')}_`
+    const priceUSD = `*USD:* _$${data.price_usd}_`
+    const priceBTC = `*BTC:* _${data.price_btc} BTC_`
+    const priceETH = `*ETH:* _${data.price_eth} ETH_`
+    const priceDelta = `*24hr Change:* _${data.cap24hrChange}%_`
+
+    const reply = `Price of *${coin}*:\n\n${rank}\n${mCap}\n${priceUSD}\n${priceBTC}\n${priceETH}\n${priceDelta}`
+
+    bot.sendMessage(chatId, reply, {parse_mode: 'markdown'}).then(() => console.log(`Found price for ${coin}`))
+  })
 })
