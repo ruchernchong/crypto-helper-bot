e44e6a498c5dbffa22732b1b8e4228971bc2f13f
Ru Chern Chong <me@ruchern.com>
2018-12-20 13:02:57 +0800

    Update prettier config
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/price.js
+++ src/price.js
@@ -10,17 +10,26 @@ axios.defaults.headers.common['X-CMC_PRO_API_KEY'] = COINMARKETCAP.API_KEY
 bot.onText(RegExp(`${prefix}mcap`), async message => {
   const chatId = message.chat.id
 
-  let data = await axios.get(`${API_BASE_URL}/v1/global-metrics/quotes/latest`).then(response => {
-    return response.data
-  })
+  let data = await axios
+    .get(`${API_BASE_URL}/v1/global-metrics/quotes/latest`)
+    .then(response => {
+      return response.data
+    })
 
-  const marketCap = `_$${parseFloat(data.data.quote.USD.total_market_cap).toLocaleString('en')}_`
-  const bitcoinDominance = `_${parseFloat(data.data.btc_dominance).toFixed(2)}%_`
-  const ethereumDominance = `_${parseFloat(data.data.eth_dominance).toFixed(2)}%_`
+  const marketCap = `_$${parseFloat(
+    data.data.quote.USD.total_market_cap
+  ).toLocaleString('en')}_`
+  const bitcoinDominance = `_${parseFloat(data.data.btc_dominance).toFixed(
+    2
+  )}%_`
+  const ethereumDominance = `_${parseFloat(data.data.eth_dominance).toFixed(
+    2
+  )}%_`
 
   const reply = `*Total Est. Market Cap (USD):* ${marketCap}\n*Bitcoin Dominance:* ${bitcoinDominance}\n*Ethereum Dominance:* ${ethereumDominance}`
 
-  bot.sendMessage(chatId, reply, { parse_mode: 'markdown' })
+  bot
+    .sendMessage(chatId, reply, { parse_mode: 'markdown' })
     .then(() => console.info('Total Market Cap in USD'))
     .catch(error => console.error(error))
 })
@@ -30,7 +39,10 @@ bot.onText(/(\$[A-Za-z]{2,})/, async (message, match) => {
   const { input } = match
   const inputSymbol = input.split('$')[1].toUpperCase()
 
-  let coin = await axios.get(`${API_BASE_URL}/v1/cryptocurrency/quotes/latest?symbol=${inputSymbol}`)
+  let coin = await axios
+    .get(
+      `${API_BASE_URL}/v1/cryptocurrency/quotes/latest?symbol=${inputSymbol}`
+    )
     .then(response => response.data.data[inputSymbol])
     .catch(error => console.error(error.message))
 
@@ -40,17 +52,26 @@ bot.onText(/(\$[A-Za-z]{2,})/, async (message, match) => {
     name = coin.name
     symbol = coin.symbol
     rank = `*Rank:* _${coin.cmc_rank}_`
-    mCap = `*Est. Market Cap (USD):* _$${parseFloat(coin.quote.USD.market_cap).toLocaleString('en')}_`
-    priceUSD = `*USD:* _$${parseFloat(coin.quote.USD.price).toLocaleString('en')}_`
-    priceDelta = `*24hr Change:* _${parseFloat(coin.quote.USD.percent_change_24h).toFixed(2)}%_ ${coin.quote.USD.percent_change_24h < 0 ? '📉' : '📈'}`
-    link = `*Link:* ${SITE_BASE_URL}/currencies/${coin.name.toLowerCase().replace(/\s+/g, '-')}`
+    mCap = `*Est. Market Cap (USD):* _$${parseFloat(
+      coin.quote.USD.market_cap
+    ).toLocaleString('en')}_`
+    priceUSD = `*USD:* _$${parseFloat(coin.quote.USD.price).toLocaleString(
+      'en'
+    )}_`
+    priceDelta = `*24hr Change:* _${parseFloat(
+      coin.quote.USD.percent_change_24h
+    ).toFixed(2)}%_ ${coin.quote.USD.percent_change_24h < 0 ? '📉' : '📈'}`
+    link = `*Link:* ${SITE_BASE_URL}/currencies/${coin.name
+      .toLowerCase()
+      .replace(/\s+/g, '-')}`
 
     reply = `💰 Here is the current price for *${name} (${symbol})*:\n\n${rank}\n${mCap}\n${priceUSD}\n${priceDelta}\n\n${link}`
   } else {
     reply = `Unable to find *${inputSymbol}*`
   }
 
-  bot.sendMessage(chatId, reply, { parse_mode: 'markdown' })
+  bot
+    .sendMessage(chatId, reply, { parse_mode: 'markdown' })
     .then(() => console.info(`Reply sent for ${inputSymbol}`))
     .catch(error => console.error(error.message))
 })
