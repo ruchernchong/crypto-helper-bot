42a3fa01f6c8373d7da19c7c1715422443009b1a
Ru Chern Chong <me@ruchern.com>
2018-12-23 00:34:32 +0800

    Clean up config
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
-'use strict'
-
-import keys from './../keys'
-import TelegramBot from 'node-telegram-bot-api'
-
 module.exports = {
-  bot: new TelegramBot(keys.TELEGRAM.TOKEN, { polling: true }),
   prefix: '!'
 }
