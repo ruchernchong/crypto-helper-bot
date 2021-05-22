df79e55f39c8f4e6c01c4950ebda2c65cc612264
Ru Chern Chong <dev@ruchern.xyz>
2021-05-22 20:58:20 +0800

    Add integration with AWS Lambda

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,4 @@
 export const API_BASE_URL: string = 'https://pro-api.coinmarketcap.com';
 export const CAL_BASE_URL: string = 'https://developers.coinmarketcal.com/v1';
+
+export const PREFIX: string = '/';
